46d681f91a14854e5e353f6142da61528f4b8c91
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-30 02:29:46 +0200

    installation de npm install @nuxtjs/axios @nuxtjs/auth-next

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,37 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@formkit/nuxt", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxtjs/tailwindcss", 
+    "nuxt-icon", 
+    "@formkit/nuxt", 
+    "@nuxtjs/i18n",
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
+  ],
   runtimeConfig: {
     public: {
       appUrl: process.env.APP_URL || "http://localhost:3000",
       apiUrl: process.env.API_URL || "http://localhost:8000",
-      
     }
   }, 
   formkit: {
-     configFile: "~/formkit.config.ts",
+    configFile: "~/formkit.config.ts",
     autoImport: true,
   },
   i18n: {
     defaultLocale: 'fr'
+  },
+  axios: {
+    baseURL: process.env.API_URL || "http://localhost:8000",
+    credentials: true
+  },
+  auth: {
+    strategies: {
+      laravelSanctum: {
+        provider: 'laravel/sanctum',
+        url: process.env.API_URL || 'http://localhost:8000'
+      }
+    }
   }
 })
